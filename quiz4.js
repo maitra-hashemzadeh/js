68df4b321256673b89c6a3e4b8903f535a39d788
maitra-hashemzadeh <mitra.hashemzadeh20022@gmail.com>
2025-03-02 19:23:20 +0330

    quiz4

---
+++ quiz4.js
@@ -0,0 +1,7 @@
+//4) Write a code to concatenate a given string n times together.(Random times between 1 and 100)
+let number = Math.random()*100
+console.log(number)
+let phone =  Math.ceil(Math.random()*100)
+console.log(phone)
+let numero =  Math.floor(Math.random()*100)
+console.log(numero)
