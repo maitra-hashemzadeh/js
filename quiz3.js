b7536e57aa4935c356800bcbb612bb8c6d778a0b
maitra-hashemzadeh <mitra.hashemzadeh20022@gmail.com>
2025-03-02 19:11:58 +0330

    quiz3

---
+++ quiz3.js
@@ -0,0 +1,5 @@
+//3) Write a code that hides email addresses by replacing the first 5 characters with ( * ) to
+// prevent unauthorized access.
+// Fitzzgerald@gmail.com => *****gerald@gmail.com
+let email = "apsignals@gmail.com"
+console.log(email.replace("apsi","****"))
