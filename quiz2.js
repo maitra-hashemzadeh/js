80e98b14fc79d07ace174145bf6d0787dc32d5e6
maitra-hashemzadeh <mitra.hashemzadeh20022@gmail.com>
2025-03-07 18:51:32 +0330

    quiz2

---
+++ quiz2.js
@@ -0,0 +1,8 @@
+//?=================================Q-2=====================================
+// Answer these questions about condition
+// todo-1:Define a variable with a number you want.
+// todo-2:then sum the variable with 5 and store it in your variable again.
+// todo-3:show the number in the console log if the variable was greater than 10
+let number = 10
+let Answer = 10+5
+console.log(Answer>=10);
