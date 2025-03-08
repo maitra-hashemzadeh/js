4becfc5f361f4b2b038c373d663b5998701cd76a
maitra-hashemzadeh <mitra.hashemzadeh20022@gmail.com>
2025-03-08 17:35:07 +0330

    quiz2

---
+++ quiz2.js
@@ -0,0 +1,24 @@
+/**
+ * /*
+ *   Array setters
+ *   -------------
+ *   WITHOUT changing the array literal declaration,
+ *   - assign the number 4 to the end of this array
+ *   - change the first value in the array to the number 1
+ *
+ * @format
+ */
+
+const numbers = [0, 1, 2, 3]; // Don't change this array literal declaration
+ const number = numbers.push(4)
+ numbers.splice(0,1,1)
+/* 
+  DO NOT EDIT BELOW THIS LINE
+  --------------------------- */
+console.log(numbers);
+
+/* 
+  EXPECTED RESULT
+  ---------------
+  [1, 1, 2, 3, 4]
+*/
