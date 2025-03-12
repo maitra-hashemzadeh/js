d0bc10a9fdc6479c3f8a6817ca7a4f85698cbd4c
maitra-hashemzadeh <mitra.hashemzadeh20022@gmail.com>
2025-03-12 21:38:59 +0330

    q8

---
+++ quiz9.js
@@ -0,0 +1,14 @@
+/** @format */
+//add comments
+// explain what this function does. you can use Google!
+
+function whatDoIDo() {
+    return Math.random() * 8 + 1;
+  } 
+//   this methood is math methood and the number is between 0&8 and add 1 to number.
+  
+  // explain what does this function do, google it
+  function whatICanDo(word1, word2) {
+    return word1.concat(word2);
+  } 
+//  this methood for concat two things together and we have diffrent varible togerher.
