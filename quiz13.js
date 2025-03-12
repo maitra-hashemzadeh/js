7cb85e87725da745bb3e0dd53f31cec6cc7f01ad
maitra-hashemzadeh <mitra.hashemzadeh20022@gmail.com>
2025-03-12 23:06:43 +0330

    q13

---
+++ quiz13.js
@@ -0,0 +1,13 @@
+//?: write a function to calculate the square root of a number
+// hint: use Math.sqrt()
+
+function squareRootFunc(num) {
+    return Math.sqrt(num);
+  }
+  
+  squareRootFunc(100); // 10
+  squareRootFunc(4); // 2
+  console.log(squareRootFunc(100));
+  console.log(squareRootFunc(4));
+  
+  
