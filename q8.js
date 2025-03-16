9a4ce2bdf7a18970ee105a1d007307acd612898c
maitra-hashemzadeh <mitra.hashemzadeh20022@gmail.com>
2025-03-16 22:29:20 +0330

    q8.join

---
+++ q8.js
@@ -0,0 +1,32 @@
+/*
+  Array methods - .join()
+  -------------------------
+  Complete the capitalize function 
+  It should return a string with the first letter in uppercase
+  For example, capitalize("hello") should return "Hello"
+  Tip: use the string method .split() and the array method .join()
+*/
+
+function capitalize(str) {
+    let person = str.split("")
+    person[0] = person[0].toUpperCase()
+    return person.join("")
+}
+
+/* 
+  DO NOT EDIT BELOW THIS LINE
+  --------------------------- */
+let sampleName = 'benyamin';
+
+console.log(capitalize(sampleName));
+console.log(capitalize('hello'));
+
+/* 
+  EXPECTED RESULT
+  ---------------
+  Benyamin
+  Hello
+*/
+
+///////////////
+// export default capitalize;
