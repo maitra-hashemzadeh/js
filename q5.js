063819dd3e1c22af96f67fecf7d3cb62cd445c62
maitra-hashemzadeh <mitra.hashemzadeh20022@gmail.com>
2025-03-16 22:12:25 +0330

    q5.sort

---
+++ q5.js
@@ -0,0 +1,24 @@
+/*
+  Array methods - sort
+  --------------------
+*/
+
+let numbers = [3, 2, 1];
+let sortedNumbers = numbers.sort(); // complete this statement
+let sortedNumbers2 = numbers.sort((a, b)=> a-b)
+/* 
+  DO NOT EDIT BELOW THIS LINE
+  --------------------------- */
+
+console.log(sortedNumbers);
+console.log(sortedNumbers2);
+
+
+/* 
+  EXPECTED RESULT
+  ---------------
+  [1, 2, 3]
+*/
+
+//////////////
+// export default sortedNumbers
